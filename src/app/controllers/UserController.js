314b8f9295697de3b766af37a8f0c5e999473ff3
Cleyson Silva <cleyson.dasilva@dieboldnixdorf.com>
2020-06-22 19:59:14 -0300

    Adicionando verificação para email e clinica na criacao do usuario

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -1,17 +1,20 @@
+import mongoose from 'mongoose';
 import User from '../models/User';
+
+const { ObjectId } = mongoose.Types;
 
 class UserController {
   async store(req, res) {
     const { email, password, name } = req.body;
-    const { clinicId } = req.headers;
+    const { clinicId } = req;
 
-    if (await User.findOne({ email, clinicid: clinicId })) {
+    const parsedClinic = new ObjectId(clinicId);
+
+    if (await User.findOne({ email, clinic: parsedClinic })) {
       return res.status(400).json({ error: 'User already exists.' });
     }
 
-    const clinic = req.clinicId;
-
-    await User.create({ name, email, password, clinic });
+    await User.create({ name, email, password, clinic: clinicId });
 
     return res.status(201).json({ name, email });
   }
